110019cdd8f98c2641e872dbc0a4f9867496389b
Tony Cronin <tonycronin@Tonys-MacBook-Pro.local>
2018-08-27 14:02:05 +0100

    added an example function to all class of tile

--- app/js/main.js
+++ app/js/main.js
@@ -50,16 +50,31 @@ var tour = {
 $(document).ready(function($) {
   // alert("js is working");
   // first child p
-  var first_P = $("#invoices p span").text('Monkeys');
+  var first_P = $(".tiles p span").text('Monkeys');
   // first child h4
-  $("#invoices").click(function(){
-    var num = $("#invoices h4 span").text();
-    var num_int = parseInt(num);
-    num_int = num_int + 1;
-    $("#invoices h4 span").text(num_int);
-    // example of changing css of child (.tile class within #invoices id)
-    $("#invoices .tile").css('border', '1px dashed green');
-    // example of selecting changing text by parents class
-    $("#invoices h4 .fas").css('color', 'green');
-  })
+  // $("#invoices").click(function(){
+  //   var num = $("#invoices h4 span").text();
+  //   var num_int = parseInt(num);
+  //   num_int = num_int + 1;
+  //   $("#invoices h4 span").text(num_int);
+  //   // example of changing css of child (.tile class within #invoices id)
+  //   $("#invoices .tile").css('border', '1px dashed green');
+  //   // example of selecting changing text by parents class
+  //   $("#invoices h4 .fas").css('color', 'green');
+  // });
+  // add a function to all class
+  $(function(){
+    $('.tiles h4 span').click(function(){
+      if($(this).data('count')) { //aleady clicked
+        $(this).data('count', $(this).data('count') + 1);
+      } else {
+        // initialise a number
+        var num = $(this).text();
+        var num_int = parseInt(num);
+        //set number and initialize
+        $(this).data('count', num_int)
+      }
+      $(this).html($(this).data('count'))
+    })
+  });
 });
