dfbfeadcf5a2cd4d689850e3a8f625541fedcba6
Tony Cronin <tonycronin@Tonys-MacBook-Pro-2.local>
2018-09-07 15:57:58 +0100

    commented out demo methods

--- app/js/main.js
+++ app/js/main.js
@@ -47,10 +47,10 @@ var tour = {
 //   content: 'Share a project you\'re proud of, a photo from a recent event, or an interesting presentation.'
 // });
 
-$(document).ready(function($) {
+// $(document).ready(function($) {
   // alert("js is working");
   // first child p
-  var first_P = $(".tiles p span").text('Monkeys');
+  // var first_P = $(".tiles p span").text('Monkeys');
   // first child h4
   // $("#invoices").click(function(){
   //   var num = $("#invoices h4 span").text();
@@ -63,23 +63,23 @@ $(document).ready(function($) {
   //   $("#invoices h4 .fas").css('color', 'green');
   // });
   // add a function to all class
-  $(function(){
-    $('.tiles').on("click", function() {
-      //save the selector as num
-      var num = $(this).find("h4 span");
-      var num_int = parseInt(num.text());
-      num_int = num_int + 1;
-      //set the count
-      $(this).find('h4 span').text(num_int);
-    });
-  });
-  // add a function to get all tiles
-  $('.tiles').bind('click', function () {
-    var sum = 0;
-    $('.tiles').each(function() {
-      sum += parseInt($(this).find("h4 span").text());
-    });
-    // here, you have your sum
-    alert("Sum of all tiles: " + sum);
-   });
-});
+//   $(function(){
+//     $('.tiles').on("click", function() {
+//       //save the selector as num
+//       var num = $(this).find("h4 span");
+//       var num_int = parseInt(num.text());
+//       num_int = num_int + 1;
+//       //set the count
+//       $(this).find('h4 span').text(num_int);
+//     });
+//   });
+//   // add a function to get all tiles
+//   $('.tiles').bind('click', function () {
+//     var sum = 0;
+//     $('.tiles').each(function() {
+//       sum += parseInt($(this).find("h4 span").text());
+//     });
+//     // here, you have your sum
+//     alert("Sum of all tiles: " + sum);
+//    });
+// });
