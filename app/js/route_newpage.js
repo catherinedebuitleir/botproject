8afdb58a531c9a9e8a04b24b6592526c4ce574a6
Rita Raher <ritaraher@Ritas-MacBook-Pro.local>
2018-09-13 15:42:15 +0100

    update

--- app/js/route_newpage.js
+++ app/js/route_newpage.js
@@ -3,6 +3,7 @@
      //use button id as your trigger (e.g id=wizardnext)
     $('#wizardnext').click(function(e){
         e.preventDefault();
+        //name the file you want to go to 
         document.location.href = 'macros.html';
     });
    
