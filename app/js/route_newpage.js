c43fc34c1ba0c05bb5ca4c32d61f974c0eca558e
Rita Raher <ritaraher@Ritas-MacBook-Pro.local>
2018-09-14 12:33:08 +0100

    updates

--- app/js/route_newpage.js
+++ app/js/route_newpage.js
@@ -8,3 +8,25 @@
     });
    
 });
+
+
+$( document ).ready(function() {
+    //use button id as your trigger (e.g id=wizardnext)
+   $('#backpage1').click(function(e){
+       e.preventDefault();
+       //name the file you want to go to 
+       document.location.href = 'index.html';
+   });
+  
+});
+
+
+//$( document ).ready(function() {
+    //use button id as your trigger (e.g id=wizardnext)
+   //$('#page3').click(function(e){
+      // e.preventDefault();
+       //name the file you want to go to 
+      // document.location.href = 'wizard3.html';
+  // });
+  
+//});
