4ea747cb1f217049c7ca22e679c38d4d94129b38
bethwalsh91@gmail.com <bethwalsh91@gmail.com>
2019-01-09 11:48:18 +0000

    Templates added

--- app/js/main.js
+++ app/js/main.js
@@ -49,42 +49,55 @@ var tour = {
 //   content: 'Share a project you\'re proud of, a photo from a recent event, or an interesting presentation.'
 // });
 
-// $(document).ready(function($) {
-  // alert("js is working");
-  // first child p
-  // var first_P = $(".tiles p span").text('Monkeys');
-  // first child h4
-  // $("#invoices").click(function(){
-  //   var num = $("#invoices h4 span").text();
-  //   var num_int = parseInt(num);
-  //   num_int = num_int + 1;
-  //   $("#invoices h4 span").text(num_int);
-  //   // example of changing css of child (.tile class within #invoices id)
-  //   $("#invoices .tile").css('border', '1px dashed green');
-  //   // example of selecting changing text by parents class
-  //   $("#invoices h4 .fas").css('color', 'green');
-  // });
-  // add a function to all class
-//   $(function(){
-//     $('.tiles').on("click", function() {
-//       //save the selector as num
-//       var num = $(this).find("h4 span");
-//       var num_int = parseInt(num.text());
-//       num_int = num_int + 1;
-//       //set the count
-//       $(this).find('h4 span').text(num_int);
-//     });
-//   });
-//   // add a function to get all tiles
-//   $('.tiles').bind('click', function () {
-//     var sum = 0;
-//     $('.tiles').each(function() {
-//       sum += parseInt($(this).find("h4 span").text());
-//     });
-//     // here, you have your sum
-//     alert("Sum of all tiles: " + sum);
-//    });
-// });\
+$(document).ready(function($) {
+
+  // Example component js
+
+  // Panel - Expandable
+  $( "#panel-show-more" ).click(function() {
+    $( "#panel-show-less" ).show();
+    $( "#panel-show-more" ).hide();
+    $( "#expanded-section" ).show();
+  });
+  
+  $( "#panel-show-less" ).click(function() {
+    $( "#panel-show-more" ).show();
+    $( "#panel-show-less" ).hide();
+    $( "#expanded-section" ).hide();
+  });
+
+  // Panel - Next Buttons
+  $( "#panel-one-next" ).click(function() {
+    $('#collapseTwo').collapse('show');
+    $( "#checkOne" ).removeClass( "not-started" );
+  });
+
+  $( "#panel-two-next" ).click(function() {
+    $('#collapseThree').collapse('show');
+    $( "#checkTwo" ).removeClass( "not-started" );
+  });
+
+  $( "#panel-three-next" ).click(function() {
+    $( "#wizard-submit" ).removeAttr("disabled");
+    $( "#checkThree" ).removeClass( "not-started" );
+  });
+
+  // Search - Expandable
+  $( "#search-show-more" ).click(function() {
+    $( "#search-show-less" ).show();
+    $( "#search-show-more" ).hide();
+    $( "#search-expanded" ).show();
+  });
+  
+  $( "#search-show-less" ).click(function() {
+    $( "#search-show-more" ).show();
+    $( "#search-show-less" ).hide();
+    $( "#search-expanded" ).hide();
+  });
+
+});
+
+
 
 
 
